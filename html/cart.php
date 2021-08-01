fdae74fa7d5ff701cb76c93f6858078e7bdea236
aditya067 <rahmanaditya067@gmail.com>
2021-08-01 22:00:56 +0700

    First

---
+++ html/cart.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Online Shop</title>
+	<link rel="stylesheet" type="text/css" href="../css/cart.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css">
+	<link href="https://fonts.googleapis.com/css2?family=Benne&display=swap" rel="stylesheet">
+<body>
+	<div class="header">		
+	<div class="container">
+		<div class="navbar">
+			<div class="logo">
+				<img src="../img/logo.png" width="120px">
+			</div>
+			<nav>
+				<ul id="MenuItems">
+					<li><a href="../html/home.html">Home</a></li>
+					<li><a href="">Produk</a></li>
+					<li><a href="">About</a></li>
+					<li><a href="">Kontak</a></li> 
+					<li><a href="">Account</a></li>
+				</ul>
+			</nav>
+		</div>
+		</div>
+
+		<!-- cart item details -->
+
+		<div class="small-container cart-page">
+			
+			<table>
+				<tr>
+					<th>Produk</th>
+					<th>Quantity</th>
+					<th>Subtotal</th>
+				</tr>
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g1.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 50.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>50.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g2.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 32.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>32.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+
+				<tr>
+					<td>
+						<div class="cart-info">
+							<img src="../img/g3.png" width="200px" alt="">
+							<div>
+								<p>Book number 1</p>
+								<small>Price: 21.000</small>
+								<br>
+								<a href="">Remove</a>
+							</div>
+						</div>
+					</td>
+
+					<td><input type="number" value="1"></td>
+					<td>21.000</td>
+				</tr>
+			</table>
+
+			<div class="total-price">
+				
+				<table>
+					<tr>
+						<td>Subtotal</td>
+						<td>100.000</td>
+					</tr>
+					<tr>
+						<td>Tax</td>
+						<td>100.000</td>
+					</tr>
+					<tr>
+						<td>Total</td>
+						<td>100.000</td>
+					</tr>
+				</table>
+
+			</div>
+
+
+
+
+
+
+
+
+
+		</div>
+	
+<!-----------------Footer----------------->
+	
+	
+	 <!-- FooterTake -->
+                        <div id="TakeWP">
+                            <div class="content">
+                                <h3 style="text-align: center;">Take Boekstad With You</h3>
+                                <p style="margin-bottom: 20px; color: #222;">
+                                    Read and write anywhere, even offline
+                                </p>
+                                <div class="buttons">
+                                    <a href="" class="tbl-pink">Start Writing</a>
+                                    <a href="" class="tbl-pink">Start Reading</a>
+                                </div>
+                                <img src="../img/footer.png">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+    
+                        <!-- Footer  -->
+                        <footer id="footer-container">
+                            <footer id="footer">
+                                <ul class="footer-list">
+                                    <li>
+                                        <a href="" style="margin-left: 90px;">Paid Stories</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Try Premium</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Get the App</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Langunge</a>
+                                    </li>
+                                    <li>
+                                        <a href="" style="margin-right: 16px;">Writers</a>
+                                    </li>
+
+                                    <li>|</li>
+
+                                    <li>
+                                        <a href="">Busines</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Press</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Paid Stories</a>
+                                    </li>
+                                </ul>
+
+                                <ul class="footer-legal">
+                                    <li>
+                                        <a href="">Terms</a>
+                                    </li>
+                                    <li>    
+                                        <a href="">Privacy</a>
+                                    </li>   
+                                    <li>
+                                        <a href="">Accessibility</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Help</a>
+                                    </li>
+                                    <li>© 2021 Boekstad</li>
+                                </ul>
+                            </footer>
+                        </footer>
+
+
+	<!--------------------Js----------------->
+
+		<script>
+			var MenuItems = document.getElementById("MenuItems");
+
+			MenuItems.style.maxHeight = "0px";
+
+			function menutoggle(){
+			if (MenuItems.style.maxHeight == "0px")
+				{
+					MenuItems.style.maxHeight = "200px"
+				}
+			else
+				{
+					MenuItems.style.maxHeight = "0px"
+				}
+			}
+		</script>
+</body>
+</html>
+
+
+
---
+++ html/login.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+    <body>
+    	<style type="text/css">
+    		#text{
+    			height: 25px;
+    			border-radius: 5px;
+    			padding: 4px;
+    			border: solid thin #aaa;
+    		}
+    		#button{
+    			padding: 10px;
+    			width: 100px;
+    			color: white;
+    			background-color: lightblue;
+    		}
+    	</style>
+    </body>
+</html>
