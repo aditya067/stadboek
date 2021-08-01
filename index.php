b6e5cc58429a4691880f3a4952d47a62ec46d11d
aditya067 <rahmanaditya067@gmail.com>
2021-08-01 22:15:01 +0700

    index nya

---
+++ index.php
@@ -0,0 +1,439 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BoekStad.</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../js/script.js" defer></script>
+    <link rel="stylesheet" href="../bootstrap_css/css/bootstrap.css">
+    <script src="../bootstrap_js/js/bootstrap.bundle.js"></script>
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo">
+                <a href=''>BoekStad.</a>
+            </div>
+            <a href="#" class="tombol-menu">
+                <span class="garis"></span>
+                <span class="garis"></span>
+                <span class="garis"></span>
+            </a>
+            <div class="menu">
+                <ul>
+                    <li><a href="" class="dropdown-toggle" data-bs-toggle="dropdown">Browse</a></li>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+
+                    <li><a href="" class="dropdown-toggle"  data-bs-toggle="dropdown">Comunity</a></li>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                    <li><a href="">Search</a></li>
+                    <li><a href=""></a></li>
+
+                    <li><a href="" class="dropdown-toggle"  data-bs-toggle="dropdown">Bahasa Indonesia</a></li>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li><a class="dropdown-item active" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                    <li><a href="../html/SignUp.php" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="wrapper">
+        <!-- untuk home -->
+        <section id="home">
+            <img src="../img/intro.png" style="margin: 100px 62px 0 0px; width: 50%;">
+            <div class="kolom">
+                <p class="deskripsi">Belajar Programming #dirumahaja</p>
+                <h2>Tetap Sehat, Tetap Semangat</h2>
+                <p class="deskripsi">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, nobis.</p><br><br>
+                <a href="../html/SignUp.php" class="tbl-pink">Start Writing</a>
+                <a href="../html/SignUp.php" class="tbl-pink">Start Reading</a>
+            </div>
+        </section>
+
+        <!-- See you container -->
+    <div class="seeyoustory">
+        <h3>See You Story...</h3>
+        <div class="itemContainer">
+            <div class="item">
+                <img src="../img/tv-icon.png" alt="">
+                Get produced to movie and film
+            </div>
+            <div class="item">
+                <img src="../img/reel-icon.png" alt="">
+                Get adapted to a Tv Series
+            </div>
+            <div class="item">
+                <img src="../img/book-icon.png">
+                Get Published
+            </div>
+        </div>
+    </div>
+
+        <!-- untuk container -->
+        <section class="container">
+            <div class="kolom">
+                <p class="deskripsi">You Will Need This</p>
+                <h2>Online Courses</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
+                <p><a href="" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
+            </div>
+            <img src="../img/gambar2.png" style="margin:  137px 51px 0px 0px ; width: 100%">
+        </section>
+
+         <section class="container left">
+            <div class="kolom">
+                <p class="deskripsi">You Will Need This</p>
+                <h2>Online Courses</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed deserunt voluptatibus possimus blanditiis reiciendis. Qui, facilis! Delectus exercitationem dolores sapiente?</p>
+                <p><a href="" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
+            </div>
+            <img src="../img/gambar2.png" style="margin:  137px 51px 0px 0px ; width: 100%">
+        </section>
+
+        <!-- Find Out More-->
+
+        <div id="FindOutMore">
+            <a href="">
+                <h4>Find out about what we do for writers</h4>
+            </a>
+        </div>
+
+        <!-- How to Works -->
+        <div class="howtoWorks">
+            <div class="container content">
+                <h2>Bagaimana BoekStad Berjalan</h2>
+                <p>Get your story discovered through the power of community and technology on BoekStad</p>
+            </div>
+           </div>
+
+
+        <!-- steps1 -->
+        <div id="steps">
+            <div class="stepContainer">
+                <div class="step">
+                    <h3 class="number" style="font-size: 100px; color: #222; line-height: normal; margin-right: 27px; font-weight: 700;">1</h3>
+                    <div>
+                        <h3 class="title" style="font-size: 25px;">Create</h3>
+                        <p class="description" style="color: #6f6f6f;">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Doloremque iste blanditiis, vitae placeat sequi aliquid, cum amet aut sed commodi ab excepturi ullam consequuntur repellendus,
+                        </p>
+                    </div>
+                </div>
+        <!-- dots1 -->
+
+        <div class="dots">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+        <!-- bubble1-->
+        <div class="bubble">
+            <div>
+                <div class="count">50+</div>
+                <div class="title">Writing Resource</div>
+            </div>
+        </div>
+    </div>
+        <!-- steps2 -->
+        <div class="stepContainer">
+                <div class="step">
+                    <h2 class="number" style="font-size: 100px; color: #222; line-height: normal; margin-right: 27px; font-weight: 700;">2</h2>
+                    <div>
+                        <h3 class="title" style="font-size: 25px;">Create</h3>
+                        <p class="description" style="color: #6f6f6f;">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Doloremque iste blanditiis, vitae placeat sequi aliquid, cum amet aut sed commodi ab excepturi ullam consequuntur repellendus,
+                        </p>
+                    </div>
+                </div>
+        <!-- dots2 -->
+
+        <div class="dots">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+        <!-- bubble2-->
+        <div class="bubble2">
+            <div>
+                <div class="count">50+</div>
+                <div class="title">Writing Resource</div>
+            </div>
+        </div>
+    </div>
+        <!-- steps3 -->
+        <div class="stepContainer">
+                <div class="step">
+                    <h3 class="number" style="font-size: 100px; color: #222; line-height: normal; margin-right: 27px; font-weight: 700;">3</h3>
+                    <div>
+                        <h3 class="title" style="font-size: 25px;">Create</h3>
+                        <p class="description" style="color: #6f6f6f;">
+                            Lorem, ipsum dolor sit amet consectetur adipisicing, elit. Doloremque iste blanditiis, vitae placeat sequi aliquid, cum amet aut sed commodi ab excepturi ullam consequuntur repellendus,
+                        </p>
+                    </div>
+                </div>
+        <!-- dots3 -->
+
+        <div class="dots">
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+            <span class="dot"></span>
+        </div>
+
+        <!-- bubble3-->
+        <div class="bubble3">
+            <div>
+                <div class="count">50+</div>
+                <div class="title">Writing Resource</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+ <!-- carousel-->
+
+        <!----- <div id="slider">
+            <input type="radio" name="slider" id="slide1" checked>
+            <input type="radio" name="slider" id="slide2">
+            <input type="radio" name="slider" id="slide3">
+            <input type="radio" name="slider" id="slide4">
+            <div id="slides">
+                <div id="overflow">
+                    <div class="inner">
+                        <div class="slide slide_1">
+                            <div class="slide-content">
+                                <h2>Slide 1</h2>
+                                <p>Content for slide 1</p>
+                            </div>
+                        </div>
+                        <div class="slide slide_2">
+                            <div class="slide-content">
+                                <h2>Slide 2</h2>
+                                <p>Content for slide 2</p>
+                            </div>
+                        </div>
+                        <div class="slide slide_3">
+                            <div class="slide-content">
+                                <h2>Slide 3</h2>
+                                <p>Content for slide 3</p>
+                            </div>
+                        </div>
+                        <div class="slide slide_4">
+                            <div class="slide-content">
+                                <h2>Slide 4</h2>
+                                <p>Content for slide 4</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="controls">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+            <div id="bullets">
+                <label for="slide1"></label>
+                <label for="slide2"></label>
+                <label for="slide3"></label>
+                <label for="slide4"></label>
+            </div>
+        </div> ---->
+
+
+        <!-- link -->
+        <div id="links">
+            <div class="GetDiscover">
+                <div class="container">
+                    <h3>Get Discovered</h3>
+                    <div class="links">
+                        <div class="link">
+                            <div class="image">
+                                <img src="../img/avatar1.png">
+                            </div>
+                            <div class="info">
+                                <h4>Lorem ipsum</h4>
+                                <p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Culpa magnam dolorem omnis, molestias aliquam obcaecati id, voluptatem ut enim fugiat, consequatur</p>
+                            </div>
+                        </div>
+
+                        <div class="link">
+                            <div class="image">
+                                <img src="../img/avatar2.png">
+                            </div>
+                            <div class="info">
+                                <h4>Lorem ipsum</h4>
+                                <p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Culpa magnam dolorem omnis, molestias aliquam obcaecati id, voluptatem ut enim fugiat, consequatur</p>
+                            </div>
+                        </div>
+
+                        <div class="link">
+                            <div class="image">
+                                <img src="../img/avatar3.png">
+                            </div>
+                            <div class="info">
+                                <h4>Lorem ipsum</h4>
+                                <p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Culpa magnam dolorem omnis, molestias aliquam obcaecati id, voluptatem ut enim fugiat, consequatur</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                </div>
+
+                        <!-- FooterTake -->
+                        <div id="TakeWP">
+                            <div class="content">
+                                <h3 style="text-align: center;">Take Boekstad With You</h3>
+                                <p style="margin-bottom: 20px; color: #222;">
+                                    Read and write anywhere, even offline
+                                </p>
+                                <div class="buttons">
+                                    <a href="" class="tbl-pink">Start Writing</a>
+                                    <a href="" class="tbl-pink">Start Reading</a>
+                                </div>
+                                <img src="../img/footer.png">
+                            </div>
+                        </div>
+                    </div>
+
+                        <!-- Footer  -->
+                        <footer id="footer-container">
+                            <footer id="footer">
+                                <ul class="footer-list">
+                                    <li>
+                                        <a href="" style="margin-left: 90px;">Paid Stories</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Try Premium</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Get the App</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Langunge</a>
+                                    </li>
+                                    <li>
+                                        <a href="" style="margin-right: 16px;">Writers</a>
+                                    </li>
+
+                                    <li>|</li>
+
+                                    <li>
+                                        <a href="">Busines</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Jobs</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Press</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Paid Stories</a>
+                                    </li>
+                                </ul>
+
+                                <ul class="footer-legal">
+                                    <li>
+                                        <a href="">Terms</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Privacy</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Accessibility</a>
+                                    </li>
+                                    <li>
+                                        <a href="">Help</a>
+                                    </li>
+                                    <li>© 2021 Boekstad</li>
+                                </ul>
+                            </footer>
+                        </footer>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
